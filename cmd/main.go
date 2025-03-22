14a139186a54530db01e459ca140614af2fee5f4
Williams Barriquero <williamsgomes45@gmail.com>
2025-03-22 00:50:58 -0300

    feat: :sparkles: trabalhando com multithreading
    RabbitMQ
    Go Routines
    Channels
    Wait Groups

--- cmd/main.go
+++ cmd/main.go
@@ -2,30 +2,64 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"sync"
 
-	"github.com/google/uuid"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/willeei/pfa-go/internal/order/infra/database"
-	orderUsecase "github.com/willeei/pfa-go/internal/order/usecase"
+	"github.com/willeei/pfa-go/internal/order/usecase"
+	"github.com/willeei/pfa-go/pkg/rabbitmq"
 )
 
 func main() {
+	maxWorkers := 3
+	wg := sync.WaitGroup{}
 	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/orders")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	repository := database.NewOrderRepository(db)
-	usecase := orderUsecase.NewCalculateFinalPriceUseCase(repository)
+	uc := usecase.NewCalculateFinalPriceUseCase(repository)
+	ch := rabbitmq.OpenChannel()
 
-	input := orderUsecase.OrderInputDTO{
-		ID:    uuid.NewString(),
-		Price: 10,
-		Tax:   1,
+	defer ch.Close()
+	out := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, out)
+
+	wg.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		defer wg.Done()
+		go worker(out, uc, i)
 	}
-	output, err := usecase.Execute(input)
-	if err != nil {
-		panic(err)
+	wg.Wait()
+
+	// input := usecase.OrderInputDTO{
+	// 	ID:    "1234",
+	// 	Price: 100,
+	// 	Tax:   10,
+	// }
+	// output, err := uc.Execute(input)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// println(output.FinalPrice)
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerId int) {
+	for msg := range deliveryMessage {
+		var input usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			fmt.Println("Error unmarshalling message", err)
+		}
+		input.Tax = 10.0
+		_, err = uc.Execute(input)
+		if err != nil {
+			fmt.Println("Error executing use case", err)
+		}
+		msg.Ack(false)
+		fmt.Println("Worker", workerId, "processed order", input.ID)
 	}
-	fmt.Printf("The Final Price is: %f\n", output.FinalPrice)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.1
 require (
 	github.com/go-sql-driver/mysql v1.9.0
 	github.com/google/uuid v1.6.0
+	github.com/rabbitmq/amqp091-go v1.10.0
 	github.com/stretchr/testify v1.10.0
 )
 
